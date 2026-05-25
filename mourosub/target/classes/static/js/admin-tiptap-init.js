dc53f6c59d5339971474fd97adaa1adeece75588
María Paz Noreña Aguirre <165820078+MeryNore@users.noreply.github.com>
2026-05-25 09:50:48 +0200

    chore: add Maven Wrapper, remove apache-maven folder, target and .vscode from tracking

--- mourosub/target/classes/static/js/admin-tiptap-init.js
+++
@@ -1,9 +0,0 @@
-/* Extracted from admin/actividades/form.html */
-// Exponer TipTap globalmente desde los módulos UMD
-  window.tiptap = { Editor: tiptap.Editor };
-  window.StarterKit = StarterKit;
-
-
-/* Extracted from admin/noticias/form.html */
-window.tiptap = { Editor: tiptap.Editor };
-  window.StarterKit = StarterKit;
