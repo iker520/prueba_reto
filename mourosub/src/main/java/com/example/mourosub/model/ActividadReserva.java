09fc9d5169f6ddfd1a1e61ab1886103bf3135d3e
María Paz Noreña Aguirre <165820078+MeryNore@users.noreply.github.com>
2026-05-25 13:07:28 +0200

    cambios apartado Reservas

--- mourosub/src/main/java/com/example/mourosub/model/ActividadReserva.java
+++ mourosub/src/main/java/com/example/mourosub/model/ActividadReserva.java
@@ -4,8 +4,6 @@ import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-
 @Entity
 @Table(name = "ACTIVIDADES_RESERVAS")
 @IdClass(ActividadReservaId.class)
